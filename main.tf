71e1439e075f49e4adeccb7b55926aa8962bcd5c
suryabhai420 <suri.munna5@gmail.com>
2019-12-12 15:12:07 +0530

    terraorm initial files12

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "ec2" {
   ami = "ami-087c2c50437d0b80d"
   instance_type = "t2.micro"
   Name = "suresh"
-  aws_iam_name = "loadbalancer"
+  awsiam_nane = "loadbalancer"
   awsiam_path = "system"
   awsiamaccesskey = "aws2_iam_user.lb.name"
   acesspolicy = "aws_iam_user.lb.name"
